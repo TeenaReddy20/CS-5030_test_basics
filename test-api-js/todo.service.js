1af2da34495c03f42c2ed66d8fa27a6dcab08b86
tkanduru <teenareddy.kanduru@slu.edu>
2022-11-07 22:53:23 -0600

    added testcases

--- test-api-js/todo.service.js
+++ test-api-js/todo.service.js
@@ -16,24 +16,35 @@ class todoservice{
     }
     constructor(){
         this.todos=this.todo_data;
+
     }
 
     get_todos(){
         return this.todos;
     }
 
-    add_todo(todo){
-        // Your code here
+    add_todo(todo){ 
+           a= {"todo":[{  
+                "title": "T2",
+                "description": "D2",
+                "done": false
+         } ]}
+            this.todo_data.todo.push(todo);
+            return(this.todos);
+            console.log(this.todos);
     }
 
     delete_todo(id){
-        // Your code here
+        
+            delete this.todo_data.todo[id];
+            this.delete_todo.splice(id, 1);
+            console.log(this.todos);
     }
 
     update_todo(id, todo){
-        // Your code here
+        this.todo_data.todo[id] = todo;
+        console.log("Update");
+        return this.todos;
     }
 }
-
-
 module.exports= todoservice;
--- test-api-js/todo.test.js
+++ test-api-js/todo.test.js
@@ -13,15 +13,38 @@ describe('todo test suite', () => {
         expect(todo_service instanceof todoservice).toBe(true);
     });
 
+});
+
     
     // Initial length of the todo list is 3 // 3 default tasks
     test("get_todos", () => {
-        expect(todo_service.get_todos().todo.length).toEqual(3);
+        expect(todoservice.get_todos().todo.length).toEqual(3);
     });
 
-    
+    describe ('todoservice', () => {
+        test('add_todo', () => {
+            todo_service.add_todo({
+                'title': 'T2',
+                'description': 'D2',
+                'done': false
+            })
+            expect(todoservice.get_todos().todo.length).toEqual(4);
+          });
 
-    // Write all your test cases here that corresponds to software requirements
+    });
+
+    test("delete_todo", () => {
+        todo_service.delete_todo(3)
+        expect(todo_service.delete_todo(5)).toEqual(false);
+    })
+
+        test("update_todo", () => {
+            expect(todoservice.update_todo(3, {
+                'title': 'Update',
+                'description': 'Updating',
+                'done': true
+            })).toEqual(false);
+
 
 
 });
